83413abffe607ef32200eb5c815be9cdbc089a93
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-07-30 10:48:58 +0300

    first commit

---
+++ src/main/java/fi/academy/drinkifyrestapi/DrinkifyRestApiApplication.java
@@ -0,0 +1,13 @@
+package fi.academy.drinkifyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkifyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkifyRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/academy/drinkifyrestapi/DrinkifyRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.academy.drinkifyrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrinkifyRestApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
