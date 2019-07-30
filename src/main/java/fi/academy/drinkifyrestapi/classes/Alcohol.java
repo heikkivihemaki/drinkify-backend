1dd5903f5a214bb65e26f505b5e91f538ef0009e
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-07-30 12:57:25 +0300

    Drink ja Alcohol luokat, repot sekä restcontrollerpohja luotu

--- src/main/java/fi/academy/drinkifyrestapi/classes/Alcohol.java
+++ src/main/java/fi/academy/drinkifyrestapi/classes/Alcohol.java
@@ -3,7 +3,7 @@ package fi.academy.drinkifyrestapi.classes;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "alcolhol")
+@Document(collection = "alcohol")
 public class Alcohol {
 
     @Id
