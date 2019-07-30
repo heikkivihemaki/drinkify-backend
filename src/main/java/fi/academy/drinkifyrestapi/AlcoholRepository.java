fa1fdbaaf49eced2ee5e9e3c94974a53f06da84e
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-07-30 15:41:04 +0300

    Item controller luotu, get (all,id,name), post, delete

--- src/main/java/fi/academy/drinkifyrestapi/AlcoholRepository.java
+++
@@ -1,7 +0,0 @@
-package fi.academy.drinkifyrestapi;
-
-import fi.academy.drinkifyrestapi.classes.Alcohol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AlcoholRepository extends MongoRepository<Alcohol, String> {
-}
