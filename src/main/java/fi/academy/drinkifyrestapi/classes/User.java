c73e7d940b0e612bb4415ef9f545ae287362f466
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-07-31 10:00:43 +0300

    vaihdettu userille email ja password

--- src/main/java/fi/academy/drinkifyrestapi/classes/User.java
+++ src/main/java/fi/academy/drinkifyrestapi/classes/User.java
@@ -15,17 +15,15 @@ public class User {
 
     @Id
     private String id;
-    private String name;
     private String email;
-    @DBRef
-    private List<Item> items;
+    private String password;
+    private List<Item> items = new ArrayList<Item>();
 
     public User(){}
 
-    public User(String name, String email) {
-        this.name = name;
+    public User(String email, String password) {
         this.email = email;
-        this.items = new ArrayList<>();
+        this.password = password;
     }
 
     public String getId() {
@@ -36,20 +34,20 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public List<Item> getItems() {
@@ -68,8 +66,8 @@ public class User {
     public String toString() {
         return "User{" +
                 "id='" + id + '\'' +
-                ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 ", items=" + items +
                 '}';
     }
